2bc3a88275ae218a4c292ed6560ee594fb5b34b9
xugongkai <xugongkai@banggood.com>
2022-02-28 17:49:26 +0800

    JavaBase Init

--- src/main/java/com/xgk/demo/App.java
+++ src/main/java/com/xgk/demo/App.java
@@ -2,16 +2,12 @@ package com.xgk.demo;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.*;
-
 /**
  * @author: xugongkai
  * @created: 2022-02-08 17:56
  */
 @Slf4j
 public class App{
-
-    private static final ExecutorService pool = Executors.newFixedThreadPool(1);
 
     public static void main(String[] args) {
         System.out.println("Hello");
