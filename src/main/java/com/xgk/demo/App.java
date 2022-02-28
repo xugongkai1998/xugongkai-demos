ca4662ab32167daa939f701d12f0f13330753ab2
xugongkai <xugongkai@banggood.com>
2022-02-28 17:48:55 +0800

    JavaBase Init

--- src/main/java/com/xgk/demo/App.java
+++ src/main/java/com/xgk/demo/App.java
@@ -1,27 +1,16 @@
 package com.xgk.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.Banner;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * @author: xugongkai
  * @created: 2022-02-08 17:56
  */
 @Slf4j
-@SpringBootApplication
-public class App implements ApplicationRunner {
+public class App{
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(App.class).bannerMode(Banner.Mode.OFF).run(args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
-        System.out.println("Hello:)");
+        System.out.println("Hello");
     }
 
 }
